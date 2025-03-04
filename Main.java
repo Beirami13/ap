64a2bffa486e1bcc2c106e2784dcea76d0a01dea
Beirami <Ahoora.beirami@gmail.com>
2025-03-04 19:58:16 +0330

    w=mg

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final double g= 9.8;
+        System.out.println("Enter your weight:");
+        Scanner input = new Scanner(System.in);
+        float m = input.nextFloat();
+        System.out.println("W is : "+(m*g));
     }
 }
