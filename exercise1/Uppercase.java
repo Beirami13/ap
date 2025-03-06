f4f7e015b9c3c745cda86ff9932d05e55b62af7f
Beirami <Ahoora.beirami@gmail.com>
2025-03-06 22:02:56 +0330

    E6.3(a)

--- exercise1/Uppercase.java
+++ exercise1/Uppercase.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class Uppercase {
     public static void main(String[] args) {
-        System.out.println("Enter numbers: ");
+        System.out.println("Enter strings: ");
         Scanner input = new Scanner(System.in);
         String st = input.nextLine();
         for (int i = 0; i < st.length() - 1; i++) {
             char c = st.charAt(i);
-            if (Character.isUpperCase(c)) {
+            if (Character.isUpperCase(c)){
                 System.out.println(c);
             }
-            input.close();
         }
+        input.close();
     }
 }
