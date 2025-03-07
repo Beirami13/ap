1dc72687059e300839e4006fc25dd2d357f89fff
Beirami <Ahoora.beirami@gmail.com>
2025-03-07 13:16:05 +0330

    E6.19

---
+++ exercise1/_Diamond_asterisks_.java
@@ -0,0 +1,30 @@
+package exercise1;
+import java.util.Scanner;
+
+public class _Diamond_asterisks_ {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the side length: ");
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                if (j == i - 1) System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                if (j == i - 1) System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
