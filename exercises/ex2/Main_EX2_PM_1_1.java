39c9cdc092f00cf6bc065835d2ddc812ba892c99
Beirami <Ahoora.beirami@gmail.com>
2025-03-24 13:01:08 +0430

    EX2_PM_1_1

---
+++ exercises/ex2/Main_EX2_PM_1_1.java
@@ -0,0 +1,26 @@
+package exercises.ex2;
+import java.util.Scanner;
+
+public class Main_EX2_PM_1_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int k = input.nextInt();
+        for (int i=0; i<k+2; i++) {
+            if (i == 0 || i == k + 1) {
+                for (int j = 0; j < k + 2; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int j = 0; j < k + 2; j++) {
+                    if (j == 0 || j == k + 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
