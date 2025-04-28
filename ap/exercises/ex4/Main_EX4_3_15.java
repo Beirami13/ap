4073cf647225bb05fb5b6299628bd7291a7423b4
Beirami <Ahoora.beirami@gmail.com>
2025-04-28 23:43:42 +0430

    EX4_3_15

---
+++ ap/exercises/ex4/Main_EX4_3_15.java
@@ -0,0 +1,37 @@
+package ap.exercises.ex4;
+
+public class Main_EX4_3_15 {
+    public static void main(String[] args) {
+        Letter myLetter = new Letter("Mary", "John");
+
+        myLetter.addLine("I am sorry we must part.");
+        myLetter.addLine("I wish you all the best.");
+
+        System.out.println(myLetter.getText());
+    }
+}
+
+class Letter {
+    private String from;
+    private String to;
+    private String body;
+
+    public Letter(String from, String to) {
+        this.from = from;
+        this.to = to;
+        this.body = "";
+    }
+
+    public void addLine(String line) {
+        body = body.concat(line).concat("\n");
+    }
+
+    public String getText() {
+        String fullText = "Dear " + to + ":\n\n";
+        fullText = fullText.concat(body);
+        fullText = fullText.concat("\nSincerely,\n\n");
+        fullText = fullText.concat(from);
+        return fullText;
+    }
+}
+
