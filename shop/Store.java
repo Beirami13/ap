de8623443af016e92d3540f2d5a81e82cb1f7c69
Beirami <Ahoora.beirami@gmail.com>
2025-05-06 11:07:10 +0430

    Shoping

--- shop/Store.java
+++ shop/Store.java
@@ -3,9 +3,20 @@ package shop;
 import java.util.ArrayList;
 
 public class Store {
-    Pen p1 = new Pen(100,"pinck","bick");
-    Pen p2 = new Pen(200,"blue","bick");
-    Book b1 = new Book(100,"melat");
-    ArrayList<Pen> p = new ArrayList<>();
-    ArrayList<Pen> b = new ArrayList<>();
+    public static void main(String[] args) {
+        Pen p1 = new Pen(100,"pinck","bick");
+        Pen p2 = new Pen(200,"blue","bick");
+        Book b1 = new Book(100,"melat");
+        ArrayList<Pen> p = new ArrayList<>();
+        ArrayList<Book> b = new ArrayList<>();
+        p.add(p1);
+        p.add(p2);
+        b.add(b1);
+        for (Pen pen : p) {
+            System.out.println(pen);
+        }
+        for (Book book : b) {
+            System.out.println(book);
+        }
+    }
 }
